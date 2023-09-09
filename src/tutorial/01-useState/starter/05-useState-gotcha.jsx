31564c67df17601da29ed69accd281770c0dfb55
Roman Khabayev <supernaturalmlady@gmail.com>
2023-09-09 21:34:26 +0600

    increasing previous value in useState

--- src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,31 @@
-const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
-};
+import { useState } from 'react'
 
-export default UseStateGotcha;
+const UseStateGotcha = () => {
+  const [number, setNumber] = useState(0)
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false)
+
+  const handleClick = () => {
+    // setNumber((currentState) => {
+    //   return currentState + 1
+    // })
+    setTimeout(() => {
+      console.log('hello')
+      setNumber((currentNumber) => {
+        return currentNumber + 1
+      })
+      setIsButtonDisabled(true)
+    }, 5000)
+  }
+
+  return (
+    <>
+      <h1>Click the button as many times as you can</h1>
+      <h2>{number}</h2>
+      <button onClick={handleClick} disabled={isButtonDisabled} className="btn">
+        click
+      </button>
+    </>
+  )
+}
+
+export default UseStateGotcha
